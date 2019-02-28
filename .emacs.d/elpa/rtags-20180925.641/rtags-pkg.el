e3671ca68f721a3dbec9d8a58eccf7c911577672
Gabriel Do <gabriel.l.do4@gmail.com>
2019-02-27 19:31:38 -0700

    added elpa

---
+++ .emacs.d/elpa/rtags-20180925.641/rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rtags" "20180925.641" "A front-end for rtags" 'nil :commit "e4072bc62f80048176117d16883bd551d1bdf7db" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "http://rtags.net")
